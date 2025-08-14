d200d5ac8a9d003d6579a66a93e3d48df98cc2cb
Kksh08 <144119063+Kksh08@users.noreply.github.com>
2025-08-14 21:54:46 +0000

    +1-1

---
+++ world/arq1.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main(){
+    for (int i = 0, j = 10; i < j; i++, j--)
+    {
+        printf(" I: %d, J: %d ", i, j);       
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    return 0;
+}
